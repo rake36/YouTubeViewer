06869775a6e3cbda8dc0187ab187e355ed800bf7
Matt Rakestraw <drakestraw@ebsco.com>
2017-12-13 08:32:41 -0500

    up to querySelector

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// ES6
 
-import App from './components/app';
-import reducers from './reducers';
+import React from 'react';          // core component stuff
+import ReactDOM from 'react-dom';   // dom manipulation stuff
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// 1. Create a new component
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// const: ES2016 - immutable/constant
+
+// no quotes?? it's ok => JSX - looks like html, but is really javascript
+
+// This is a type, not an instance
+// function() => () ES6 syntax, which changes semantics of 'this'
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+// 2. inject this component into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+// querySelector does not seem to apply to all elements...
