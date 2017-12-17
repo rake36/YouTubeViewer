704a148332e42931ae7ba68c6d25b50e440ec2b6
Matt Rakestraw <rake36@gmail.com>
2017-12-17 09:50:37 -0500

    finish implementing youtube search using callbacks and no redux

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -33,17 +33,18 @@ class SearchBar extends Component {
         // if value being set by state, then this is a controlled input element
         //  onChange re-renders the component due to the setState call, which then allows value to read from state
         return (
-            <div>
-                <input 
-                    value = {this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
 
-    // onInputChange(event) {
-    //     console.log(event.target.value);
-    // }
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChanged(term);
+    }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ require('dotenv').config()
 // 1 component per file!
 
 
-
-import React from 'react';          // core component stuff
+import _ from 'lodash';
+import React, { Component } from 'react';          // core component stuff
 import ReactDOM from 'react-dom';   // dom manipulation stuff
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // npm install --save youtube-api-search
 //  create .env file with appropriate settings
@@ -19,9 +21,7 @@ import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyCPfClJS0Ehqc8C3Jq9FEZx0ipAhC5bUM8'; //process.env.REACT_APP_YOUTUBE_API_KEY;
 // showProcessEnv();
 
-YTSearch({key: API_KEY, term: 'algorithms'}, function(data){
-    console.log(data);
-});
+
 
 // 1. Create a new component
 
@@ -32,19 +32,62 @@ YTSearch({key: API_KEY, term: 'algorithms'}, function(data){
 // This is a type, not an instance
 // function() => () ES6 syntax, which changes semantics of 'this'
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
-};
+// functional form of App
+// const App = () => {
+//     return (
+//     <div>
+//         <SearchBar />
+//     </div>
+//     );
+// };
+
+// We would rather have class form of App so we can keep track of state
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('algorithms');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            // console.log(videos);
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0] // set to first video we retrieve
+            });
+            // this.setState({ videos });  // can do this if key and object are the same spelling
+        });
+    }
+
+    render() {
+
+        // every 300ms
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChanged={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos}
+                />
+            </div>
+        );
+    }
+}
 
 // 2. inject this component into the DOM
 // querySelector does not seem to apply to all elements...
-ReactDOM.render(<App />, document.querySelector('.container'));  
+ReactDOM.render(<App />, document.querySelector('.container'));
 
-function showProcessEnv() {
-    console.log(process.env);
-}
+// function showProcessEnv() {
+//     console.log(process.env);
+// }
 
